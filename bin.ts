4ef973a8da347f8fdbff9ccda7f61a1f588e0645
bkerr <bk7987@gmail.com>
2023-09-14 10:23:52 -0500

    Handle backslashes better

--- bin.ts
+++ bin.ts
@@ -9,78 +9,78 @@ import { Handler, isHandlerFile } from "./handler";
  * @returns The path to the routes folder.
  */
 function getRoutesDirPath() {
-  const arg = process.argv[2];
-  if (!arg || arg.length < 1) {
-    throw new Error("No argument supplied for routes directory!");
-  } else if (!isDir(arg)) {
-    throw new Error(`Path ${arg} is not a directory.`);
-  }
-  return arg.trim().toLowerCase();
+	const arg = process.argv[2];
+	if (!arg || arg.length < 1) {
+		throw new Error("No argument supplied for routes directory!");
+	} else if (!isDir(arg)) {
+		throw new Error(`Path ${arg} is not a directory.`);
+	}
+	return arg.trim().toLowerCase();
 }
 
 /**
  * Recursively returns all handler file paths in a given directory.
  */
 function getHandlerFilePaths(baseDir: string): string[] {
-  const filePaths: string[] = [];
-  const items = fs.readdirSync(baseDir);
+	const filePaths: string[] = [];
+	const items = fs.readdirSync(baseDir);
 
-  for (const item of items) {
-    if (isDir(path.join(baseDir, item))) {
-      filePaths.push(...getHandlerFilePaths(path.join(baseDir, item)));
-    } else if (isHandlerFile(item)) {
-      filePaths.push(path.join(baseDir, item));
-    }
-  }
+	for (const item of items) {
+		if (isDir(path.join(baseDir, item))) {
+			filePaths.push(...getHandlerFilePaths(path.join(baseDir, item)));
+		} else if (isHandlerFile(item)) {
+			filePaths.push(path.join(baseDir, item));
+		}
+	}
 
-  return filePaths;
+	return filePaths;
 }
 
 function writeFile(filePath: string, contents: string) {
-  fs.writeFileSync(filePath, contents);
+	fs.writeFileSync(filePath, contents);
 }
 
 function createRoutesFile(routesDirPath: string) {
-  const handlerFilePaths = getHandlerFilePaths(routesDirPath);
+	const handlerFilePaths = getHandlerFilePaths(routesDirPath);
 
-  const handlers: Handler[] = [];
-  for (const filePath of handlerFilePaths) {
-    handlers.push(new Handler(filePath, routesDirPath));
-  }
+	const handlers: Handler[] = [];
+	for (const filePath of handlerFilePaths) {
+		handlers.push(new Handler(filePath, routesDirPath));
+	}
 
-  const hasRoutesWithParams = handlers.some((h) => !!h.params.length);
+	const hasRoutesWithParams = handlers.some((h) => !!h.params.length);
 
-  const imports = `import type { ApiRouteProps } from 'sst/constructs';\n\n`;
+	const imports = `import type { ApiRouteProps } from 'sst/constructs';\n\n`;
 
-  const routeMap = `const routeMap = {\n${handlers.reduce<string>(
-    (acc, handler) =>
-      acc.concat(
-        `\t'${handler.pathString()}': { params: [${handler.params
-          .map((param) => `'${param}'`)
-          .join(", ")}], path: '${handler.handlerPath}', },\n`
-      ),
-    ""
-  )}} as const;\n\n`;
+	const routes = `const routes = {\n${handlers.reduce<string>(
+		(acc, handler) =>
+			acc.concat(
+				`\t'${handler.pathString()}': { params: [${handler.params
+					.map((param) => `'${param}'`)
+					.join(", ")}], path: '${handler.handlerPath}', },\n`,
+			),
+		"",
+	)}} as const;\n\n`;
 
-  const routeType = `export type Route = keyof typeof routeMap;\n\n`;
+	const routeType = `export type Route = keyof typeof routes;\n\n`;
 
-  const paramRouteType = `type ParamRoute = keyof {
-  [R in Route as (typeof routeMap)[R]['params'] extends readonly [] ? never : R]: (typeof routeMap[R]);
+	const paramRouteType = `type ParamRoute = keyof {
+  [R in Route as (typeof routes)[R]['params'] extends readonly [] ? never : R]: (typeof routes[R]);
 };\n\n`;
 
-  const routeParamsType = `type RouteParams<R extends ParamRoute> = {
+	const routeParamsType = `type RouteParams<R extends ParamRoute> = {
   [RouteName in ParamRoute]: {
-    [K in (typeof routeMap)[R]['params'][number]]: string;
+    [K in (typeof routes)[R]['params'][number]]: string;
   };
 }[R];\n\n`;
 
-  const eventWithPathParametersType = `type EventWithPathParameters = {
+	const eventWithPathParametersType = `type EventWithPathParameters = {
   pathParameters?: {
     [name: string]: string | undefined;
   };
 };\n\n`;
 
-  const getPathParametersFunction = `export function getPathParameters<R extends ParamRoute>(
+	const getPathParametersFunction = `export function getPathParameters<R extends ParamRoute>(
   route: R,
   event: EventWithPathParameters
 ): RouteParams<R> {
@@ -89,20 +89,20 @@ function createRoutesFile(routesDirPath: string) {
   }
 
   const params: Record<string, string> = {};
-  for (const param of routeMap[route].params) {
+  for (const param of routes[route].params) {
     if (event.pathParameters[param]) {
       params[param] = event.pathParameters[param]!;
     }
   }
   
-  if (routeMap[route].params.length !== Object.keys(params).length) {
+  if (routes[route].params.length !== Object.keys(params).length) {
     throw new Error('Could not find all required path parameters.');
   }
   
   return params as RouteParams<R>;
 }\n\n`;
 
-  const routeConfigType = `type RouteConfig = Record<Route, ApiRouteProps<string>> & {
+	const routeConfigType = `type RouteConfig = Record<Route, ApiRouteProps<string>> & {
   route(
     route: Route,
     configFn: (current: ApiRouteProps<string>) => ApiRouteProps<string>,
@@ -110,8 +110,8 @@ function createRoutesFile(routesDirPath: string) {
   toConfig(): Record<Route, ApiRouteProps<string>>;
 };\n\n`;
 
-  const routeConfigBuild = `export const routeConfig: RouteConfig = {} as RouteConfig;
-for (const [name, { path }] of Object.entries(routeMap)) {
+	const routeConfigBuild = `export const routeConfig: RouteConfig = {} as RouteConfig;
+for (const [name, { path }] of Object.entries(routes)) {
   routeConfig[name as Route] = path;
 }
 
@@ -125,25 +125,25 @@ routeConfig.toConfig = function() {
   return this;
 };\n`;
 
-  let fileContents = "".concat(imports, routeMap, routeType);
+	let fileContents = "".concat(imports, routes, routeType);
 
-  if (hasRoutesWithParams) {
-    fileContents = fileContents.concat(
-      paramRouteType,
-      routeParamsType,
-      eventWithPathParametersType,
-      getPathParametersFunction
-    );
-  }
+	if (hasRoutesWithParams) {
+		fileContents = fileContents.concat(
+			paramRouteType,
+			routeParamsType,
+			eventWithPathParametersType,
+			getPathParametersFunction,
+		);
+	}
 
-  fileContents = fileContents.concat(routeConfigType, routeConfigBuild);
+	fileContents = fileContents.concat(routeConfigType, routeConfigBuild);
 
-  writeFile(path.join(routesDirPath, "routes.ts"), fileContents);
+	writeFile(path.join(routesDirPath, "routes.ts"), fileContents);
 }
 
 export function main() {
-  const routesDirPath = getRoutesDirPath();
-  createRoutesFile(routesDirPath);
+	const routesDirPath = getRoutesDirPath();
+	createRoutesFile(routesDirPath);
 }
 
 main();
--- handler.ts
+++ handler.ts
@@ -2,90 +2,79 @@ import * as path from "path";
 import { getSlashCount } from "./util";
 
 const handlerFileNames = new Set([
-  "get.ts",
-  "post.ts",
-  "patch.ts",
-  "options.ts",
-  "delete.ts",
-  "put.ts",
+	"get.ts",
+	"post.ts",
+	"patch.ts",
+	"options.ts",
+	"delete.ts",
+	"put.ts",
 ] as const);
 type HandlerFileName = typeof handlerFileNames extends Set<infer R> ? R : never;
 
 export function isHandlerFile(fileName: string): fileName is HandlerFileName {
-  return handlerFileNames.has(fileName as HandlerFileName);
+	return handlerFileNames.has(fileName as HandlerFileName);
 }
 
-const handlerMethods = new Set([
-  "GET",
-  "POST",
-  "PUT",
-  "DELETE",
-  "OPTIONS",
-  "PATCH",
-] as const);
-export type HandlerMethod = typeof handlerMethods extends Set<infer R>
-  ? R
-  : never;
+const handlerMethods = new Set(["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"] as const);
+export type HandlerMethod = typeof handlerMethods extends Set<infer R> ? R : never;
 
 export function isHandlerMethod(method: string): method is HandlerMethod {
-  return handlerMethods.has(method as HandlerMethod);
+	return handlerMethods.has(method as HandlerMethod);
 }
 
 export class Handler {
-  public readonly filePath: string;
-  public readonly handlerPath: string;
-  public readonly params: string[] = [];
-  public readonly method: HandlerMethod;
+	public readonly filePath: string;
+	public readonly handlerPath: string;
+	public readonly params: string[] = [];
+	public readonly method: HandlerMethod;
 
-  private readonly rootPath: string;
+	private readonly rootPath: string;
 
-  constructor(filePath: string, rootPath: string) {
-    this.filePath = filePath;
-    this.rootPath = rootPath;
+	constructor(filePath: string, rootPath: string) {
+		this.filePath = filePath;
+		this.rootPath = rootPath.replace(/[\/\\]/g, "/");
 
-    /**
-     * Set the handler path.
-     */
-    this.handlerPath = this.filePath
-      .slice(0, this.filePath.length - 3)
-      .concat(".handler");
+		/**
+		 * Set the handler path.
+		 */
+		this.handlerPath = this.filePath
+			.slice(0, this.filePath.length - 3)
+			.replace(/[\/\\]/g, "/")
+			.concat(".handler");
 
-    /**
-     * Set the handler method.
-     */
-    const method = this.filePath
-      .slice(this.filePath.lastIndexOf("/") + 1, this.filePath.length - 3)
-      .toUpperCase();
-    if (!isHandlerMethod(method)) {
-      throw new Error(`Invalid handler file method: ${method}`);
-    }
-    this.method = method;
+		/**
+		 * Set the handler method.
+		 */
+		const method = this.handlerPath
+			.slice(this.handlerPath.lastIndexOf("/") + 1, this.handlerPath.length - 8)
+			.toUpperCase();
+		if (!isHandlerMethod(method)) {
+			throw new Error(`Invalid handler file method: ${method}`);
+		}
+		this.method = method;
 
-    /**
-     * Parse out handler path parameters.
-     */
-    const matches = this.handlerPath.matchAll(/{(.*?)}/g);
-    const used = new Set<string>();
+		/**
+		 * Parse out handler path parameters.
+		 */
+		const matches = this.handlerPath.matchAll(/{(.*?)}/g);
+		const used = new Set<string>();
 
-    for (const match of matches) {
-      if (used.has(match[1])) {
-        throw new Error(
-          `A path parameter appeared twice in a handler path. Parameter: ${match[1]} Path: ${this.handlerPath}`
-        );
-      }
-      this.params.push(match[1]);
-      used.add(match[1]);
-    }
-  }
+		for (const match of matches) {
+			if (used.has(match[1])) {
+				throw new Error(
+					`A path parameter appeared twice in a handler path. Parameter: ${match[1]} Path: ${this.handlerPath}`,
+				);
+			}
+			this.params.push(match[1]);
+			used.add(match[1]);
+		}
+	}
 
-  public pathString(): string {
-    const urlPath = this.handlerPath.replace(this.rootPath, "");
-    const lastSlashPosition = urlPath.lastIndexOf("/");
-    const slashCount = getSlashCount(urlPath);
-    const sliceOffset = slashCount === 1 ? 1 : 0;
-    return `${this.method} ${urlPath.slice(
-      urlPath.indexOf("/"),
-      lastSlashPosition + sliceOffset
-    )}`;
-  }
+	public pathString(): string {
+		const urlPath = this.handlerPath.replace(this.rootPath, "");
+		const lastSlashPosition = urlPath.lastIndexOf("/");
+		const slashCount = getSlashCount(urlPath);
+		const sliceOffset = slashCount === 1 ? 1 : 0;
+		return `${this.method} ${urlPath.slice(urlPath.indexOf("/"), lastSlashPosition + sliceOffset)}`;
+	}
 }
